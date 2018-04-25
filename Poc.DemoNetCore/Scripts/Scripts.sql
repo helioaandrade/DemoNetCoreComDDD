bbba68a20c9a32eeb83aa56b25f672aab1e80a56
Hélio Andrade <helioandrade@hotmail.com>
2018-04-24 23:10:15 -0300

    Atualização script BD

--- Poc.DemoNetCore/Scripts/Scripts.sql
+++ Poc.DemoNetCore/Scripts/Scripts.sql
@@ -2,23 +2,19 @@
 -- Script de criação de tabela do banco Demonetcore no Sqlite
 
 CREATE TABLE Pessoa (
-    Id        INTEGER      PRIMARY KEY AUTOINCREMENT,
-    Nome      VARCHAR (50) NOT NULL,
-    Latitude  DOUBLE       NOT NULL,
-    Longitude DOUBLE       NOT NULL
+    Id        INTEGER         PRIMARY KEY AUTOINCREMENT,
+    Nome      VARCHAR (50)    NOT NULL,
+    Latitude  DATETIME (2, 9) NOT NULL,
+    Longitude DECIMAL (2, 9)  NOT NULL
 );
 
-CREATE TABLE CalculoHistoricoLog (
-    Id        INTEGER  PRIMARY KEY AUTOINCREMENT,
-    PessoaId  INT      NOT NULL,
-    Distancia DOUBLE  NOT NULL,
-    DataHora  DATETIME
-);
 
 CREATE TABLE CalculoHistoricoLog (
-    Id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    PessoaOrigemId  INT     NOT NULL,
-    PessoaDestinoId INT     NOT NULL,
-    Distancia       DOUBLE NOT NULL
+    Id                INTEGER  PRIMARY KEY AUTOINCREMENT,
+    PessoaOrigemId    INT      NOT NULL,
+    PessoaDestinoId   INT      NOT NULL,
+    Distancia         DOUBLE   NOT NULL,
+    UltimaAtualizacao DATETIME DEFAULT (CURRENT_TIMESTAMP) 
 );
+
 
